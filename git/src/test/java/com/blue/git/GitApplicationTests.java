8b68cca5547f6c7136f5f80ff812fc1f2a722881
sunjianfeng <sunjianfeng@baozheng.cc>
2020-05-30 09:28:01 +0800

    add @RestController

--- git/src/test/java/com/blue/git/GitApplicationTests.java
+++ git/src/test/java/com/blue/git/GitApplicationTests.java
@@ -2,7 +2,7 @@ package com.blue.git;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+//@RestController
 @SpringBootTest
 class GitApplicationTests {
 
